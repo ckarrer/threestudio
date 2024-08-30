8b2b29c889f04348acb4dc14dfc96e8a55f2a43e
ckarrer <47241179+ckarrer@users.noreply.github.com>
2024-08-30 13:30:05 +0200

    Add logs

--- launch.py
+++ launch.py
@@ -86,6 +86,7 @@ def load_custom_modules():
                 continue
             time_before = time.perf_counter()
             success = load_custom_module(module_path)
+            print("load custom module " + module_path + " with success=" + success)
             node_import_times.append(
                 (time.perf_counter() - time_before, module_path, success)
             )
