794bca0146b23dfe35079061a8d2fb408d1beb31
ckarrer <47241179+ckarrer@users.noreply.github.com>
2024-09-10 09:32:10 +0200

    Create setup_extensions.py

---
+++ docker/setup_extensions.py
@@ -0,0 +1,14 @@
+import git
+import os
+
+repos = [
+    "https://github.com/ckarrer/threestudio-3dgs.git",
+    "https://github.com/ashawkey/diff-gaussian-rasterization.git",
+    "https://github.com/DSaurus/simple-knn.git"
+]
+
+os.chdir("~/threestudio/custom")
+git.Repo.clone_from("https://github.com/ckarrer/threestudio-3dgs.git", "threestudio-3dgs")
+os.chdir("threestudio-3dgs")
+git.Repo.clone_from("https://github.com/ashawkey/diff-gaussian-rasterization.git")
+git.Repo.clone_from("https://github.com/DSaurus/simple-knn.git")
